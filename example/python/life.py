37cd1ebae0712d08ba3a29ac15c9c899c762fd0c
David Lechner <david@lechnology.com>
2016-05-16 18:46:48 -0500

    Add an example python file
    It works, but it is *really* slow.

---
+++ example/python/life.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+#
+# Conway's Game of Life
+#
+# FIXME: This program works, but it is *really* slow!
+#
+
+from gi.repository import GLib
+from gi.repository import Grx
+from random import random
+
+
+class Life:
+    def __init__(self):
+        self.source_id = 0
+        self.width = 0
+        self.height = 0
+        self.old_state = None
+        self.new_state = None
+        self.color = None
+
+    def run(self):
+        main_loop = GLib.MainLoop()
+
+        if not Grx.set_mode_default_graphics(True):
+            raise IOError('Could not set graphics mode')
+        self.width = Grx.get_size_x()
+        self.height = Grx.get_size_y()
+        self.old_state = [[0 for y in range(self.height)] for x in range(self.width)]
+        self.new_state = [[0 for y in range(self.height)] for x in range(self.width)]
+        self.color = (Grx.color_info_get_black(), Grx.color_info_get_white())
+
+        Grx.set_screen_active_callback(self.screen_active_handler)
+        self.screen_active_handler(Grx.is_screen_active())
+
+        self.randomize()
+        main_loop.run()
+
+    def randomize(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                on = random() > 0.82
+                self.old_state[x][y] = on
+                if on:
+                    Grx.draw_point_nc(x, y, self.color[on])
+
+    def update_state(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                live_count = (self.old_state[(x - 1) % self.width][(y - 1) % self.height] +
+                              self.old_state[x][(y - 1) % self.height] +
+                              self.old_state[(x + 1) % self.width][(y - 1) % self.height] +
+                              self.old_state[(x - 1) % self.width][y] +
+                              self.old_state[(x + 1) % self.width][y] +
+                              self.old_state[(x - 1) % self.width][(y + 1) % self.height] +
+                              self.old_state[x][(y + 1) % self.height] +
+                              self.old_state[(x + 1) % self.width][(y + 1) % self.height])
+                self.new_state[x][y] = (live_count | self.old_state[x][y]) == 3
+
+        self.old_state, self.new_state = self.new_state, self.old_state
+
+    def draw(self):
+
+        self.update_state()
+        for y in range(self.height):
+            for x in range(self.width):
+                Grx.draw_point_nc(x, y, self.color[self.old_state[x][y]])
+
+        return GLib.SOURCE_CONTINUE
+
+    def screen_active_handler(self, active):
+        """
+        This is to handle console switching. It basically pauses the drawing loop when this is not the active console.
+
+        :param active: True if the screen is active and we can draw on it
+        """
+        if active:
+            self.source_id = GLib.idle_add(self.draw)
+        else:
+            GLib.source_remove(self.source_id)
+
+if __name__ == '__main__':
+    life = Life()
+    life.run()
