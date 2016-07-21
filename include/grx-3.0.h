acb20b6b75c798665f1a4f1f9ed2ea90d60761f3
David Lechner <david@lechnology.com>
2016-07-21 12:47:19 -0500

    alphabatize includes

--- include/grx-3.0.h
+++ include/grx-3.0.h
@@ -20,27 +20,27 @@
 #ifndef __GRX_3_0_H_INCLUDED__
 #define __GRX_3_0_H_INCLUDED__
 
+#include <grx/application.h>
 #include <grx/color.h>
-#include <grx/mode.h>
 #include <grx/context.h>
-#include <grx/extents.h>
-#include <grx/draw.h>
-#include <grx/draw_nc.h>
-#include <grx/text.h>
-#include <grx/wideline.h>
-#include <grx/pattern.h>
-#include <grx/image.h>
-#include <grx/user.h>
-#include <grx/mouse.h>
-#include <grx/gformats.h>
-#include <grx/utils.h>
-#include <grx/events.h>
-#include <grx/input_keys.h>
-#include <grx/input_keysyms.h>
 #include <grx/device.h>
 #include <grx/device_manager.h>
-#include <grx/application.h>
+#include <grx/draw.h>
+#include <grx/draw_nc.h>
 #include <grx/error.h>
+#include <grx/events.h>
+#include <grx/extents.h>
 #include <grx/frame_mode.h>
+#include <grx/gformats.h>
+#include <grx/image.h>
+#include <grx/input_keys.h>
+#include <grx/input_keysyms.h>
+#include <grx/mode.h>
+#include <grx/mouse.h>
+#include <grx/pattern.h>
+#include <grx/text.h>
+#include <grx/user.h>
+#include <grx/utils.h>
+#include <grx/wideline.h>
 
 #endif  /* __GRX_3_0_H_INCLUDED__ */
