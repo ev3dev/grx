33d13d72fecb6d3977878ff7a2e5b50f0990d0b3
David Lechner <david@lechnology.com>
2017-03-15 23:53:22 -0500

    Add dockerfiles for setting up cross-compiling environment

---
+++ docker/armel.dockerfile
@@ -0,0 +1,21 @@
+FROM ev3dev/debian-jessie-armel-cross
+
+RUN sudo apt-get update && \
+    DEBIAN_FRONTEND=noninteractive sudo apt-get install --yes --no-install-recommends \
+        cmake \
+        gir1.2-glib-2.0 \
+        gobject-introspection \
+        gtk-doc-tools \
+        libfontconfig1-dev \
+        libfreetype6-dev \
+        libgirepository1.0-dev \
+        libglib2.0-dev \
+        libgtk-3-dev \
+        libgudev-1.0-dev \
+        libinput-dev \
+        libjpeg-dev \
+        libpng-dev \
+        libudev-dev \
+        libxkbcommon-dev \
+        pkg-config \
+        valac
---
+++ docker/setup.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+set -e
+
+project="grx"
+
+script_dir=$(dirname $(readlink -f "${0}"))
+
+build_dir="${1}"
+if [ ! -n "${build_dir}" ]; then
+    echo "Error: Must specify build directory"
+    exit 1
+fi
+
+case ${2} in
+    armel|armhf)
+        arch=${2}
+        ;;
+    *)
+        echo "Error: Must specify 'armel' or 'armhf'"
+        exit 1
+        ;;
+esac
+
+if ! which docker >/dev/null; then
+    echo "Error: Docker is not installed"
+    exit 1
+fi
+
+image_name="${project}-${arch}"
+container_name="${project}_${arch}"
+
+docker build \
+    --tag ${image_name} \
+    --no-cache \
+    --file "${script_dir}/${arch}.dockerfile" \
+    "${script_dir}/"
+mkdir -p ${build_dir}
+
+docker rm --force ${container_name} >/dev/null 2>&1 || true
+docker run \
+    --volume "${build_dir}:/build" \
+    --volume "$(pwd):/src" \
+    --workdir /build \
+    --name ${container_name} \
+    --env "TERM=${TERM}" \
+    --env "DESTDIR=/build/dist" \
+    --tty \
+    --detach \
+    ${image_name} tail
+
+docker exec --tty ${container_name} cmake /src \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_TOOLCHAIN_FILE=/opt/gcc-linaro-5.3-2016.02-x86_64_arm-linux-gnueabihf/toolchain.cmake
+
+echo "Done. You can now compile by running 'docker exec --tty ${container_name} make'"
