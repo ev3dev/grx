b28e03324a39c6ebff9b736fa6d6020d2ae0ebf5
David Lechner <david@lechnology.com>
2017-07-02 17:15:47 -0500

    Get fresh clone building on macOS

--- setup.cmake
+++ setup.cmake
@@ -4,7 +4,11 @@ execute_process (
     COMMAND ${CMAKE_COMMAND} -E make_directory build
 )
 
+if (APPLE)
+    set(appleOptions "-DGRX_ENABLE_DOC=No")
+endif ()
+
 execute_process (
-    COMMAND ${CMAKE_COMMAND} -DCMAKE_BUILD_TYPE=Debug ..
+    COMMAND ${CMAKE_COMMAND} -DCMAKE_BUILD_TYPE=Debug ${appleOptions} ..
     WORKING_DIRECTORY build
 )
