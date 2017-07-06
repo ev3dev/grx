f91f332712cd9878402ff67b3b3b1191be05e234
David Lechner <david@lechnology.com>
2017-07-06 13:48:05 -0500

    Build valadoc

--- doc/sphinx/api_docs.rst
+++ doc/sphinx/api_docs.rst
@@ -7,6 +7,7 @@ The following API docs are available:
 * `C API <c-api/index.html>`_
 * `JavaScript (Gjs) API <gjs-api/index.html>`_
 * `Python API <python-api/index.html>`_
+* `Vala API <vala-api/grx-3.0/index.htm>`_
 
 .. note:: The C API docs are organized nicely and have additional notes, so even if you
     are using another programming language, you may find it useful to refer to the
--- release-doc.sh
+++ release-doc.sh
@@ -10,15 +10,18 @@ build_dir=${base_dir}/build
 rm -rf ${base_dir}/doc/sphinx/_html_extra/c-api
 rm -rf ${base_dir}/doc/sphinx/_html_extra/gjs-api
 rm -rf ${base_dir}/doc/sphinx/_html_extra/python-api
+rm -rf ${base_dir}/doc/sphinx/_html_extra/vala-api
 
 mkdir -p ${base_dir}/doc/sphinx/_html_extra/c-api
 mkdir -p ${base_dir}/doc/sphinx/_html_extra/gjs-api
 mkdir -p ${base_dir}/doc/sphinx/_html_extra/python-api
+mkdir -p ${base_dir}/doc/sphinx/_html_extra/vala-api
 
 rm -rf ${build_dir}
 cmake -D CMAKE_BUILD_TYPE=Release -D GTKDOC_REBASE_ONLINE=Yes -B${build_dir} -H${base_dir}
-make -C ${build_dir} doc
+make -s -C ${build_dir} doc
 
 cp -R ${build_dir}/src/grx/html/* ${base_dir}/doc/sphinx/_html_extra/c-api
 yelp-build html -o ${base_dir}/doc/sphinx/_html_extra/gjs-api ${build_dir}/doc/yelp/gjs
 yelp-build html -o ${base_dir}/doc/sphinx/_html_extra/python-api ${build_dir}/doc/yelp/python
+cp -R ${build_dir}/doc/valadoc/valadoc/* ${base_dir}/doc/sphinx/_html_extra/vala-api
